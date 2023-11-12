0bf3835643a3b88ea4a12ff8d2f61e8e0d4bf73f
Victor T <victordevt@gmail.com>
2023-11-12 21:43:16 +0200

    init file separate, full ex for java

--- vthreads_java/src/main/java/org/example/Main.java
+++ vthreads_java/src/main/java/org/example/Main.java
@@ -7,35 +7,59 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-//        runClassicThreads();
-        runVirtualThreads();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        runClassicThreads();
+//        runVirtualThreads();
     }
 
-    private static void runClassicThreads() {
-        int i = 0;
-        for (i = 0; i < 100_000; i++) {
-            new Thread(() -> {
+    private static void runClassicThreads() throws IOException {
+        String filePath = "./vthreads.txt";
+        int nrThreads = 3000;
+        Thread[] threads = new Thread[nrThreads];
+
+        initFile(filePath);
+
+        for (int i = 0; i < nrThreads; i++) {
+            int thdIdx = i;
+            threads[i] = new Thread(() -> {
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
+                    appendToFile(filePath, "Platform Thread " + thdIdx + "\n");
+                } catch (InterruptedException | IOException e) {
                     throw new RuntimeException(e);
                 }
-            }).start();
+            });
         }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return;
     }
 
-    private static void runVirtualThreads() {
+    private static void runVirtualThreads() throws IOException {
 
         String filePath = "./vthreads.txt";
-        int nrThreads = 100_000;
+        int nrThreads = 3000;
         Thread[] virtualThreads = new Thread[nrThreads];
+
+        initFile(filePath);
+
         for (int i = 0; i < nrThreads; i++) {
-            int threadNumber = i;
+            int thdIdx = i;
             virtualThreads[i] = Thread.ofVirtual().start(() -> {
                 try {
-                    appendToFile(filePath, "Virtual Thread " + threadNumber + '\n');
-                } catch (IOException e) {
+                    Thread.sleep(1000);
+                    appendToFile(filePath, "Virtual Thread " + thdIdx + '\n');
+                } catch (IOException | InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             });
@@ -49,25 +73,29 @@ public class Main {
             }
         }
 
-//        int i = 0;
-//        appendToFile("./vthreads.txt", "ok");
-//        for (i = 0; i < 100_000; i++) {
-//            Thread thread = Thread.ofVirtual().name("virtual thread - " + i).start(() -> {
-//                try {
-//                    Thread.sleep(1000);
-//                    appendToFile("./vthreads.txt", Thread.currentThread().getName());
-//                } catch (InterruptedException e) {
-//                    System.out.println("Exc");
-//                    throw new RuntimeException(e);
-//                }
-//            });
-//        }
-//        i = 0;
+        return;
     }
 
     public static void appendToFile(String filePath, String textToAppend) throws IOException {
         Path path = Path.of(filePath);
 
+
+        try {
+            Thread.sleep(1000); // 1000 milliseconds = 1 second
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            // Handle exception appropriately, e.g., log it or throw it
+        }
+
+        try {
+            Files.write(path, textToAppend.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void initFile(String filePath) throws IOException {
+        Path path = Path.of(filePath);
         if (!Files.exists(path)) {
             try {
                 Files.createFile(path);
@@ -81,19 +109,6 @@ public class Main {
 
             // Close the FileWriter to save changes
             fileWriter.close();
-        }
-
-        try {
-            Thread.sleep(1000); // 1000 milliseconds = 1 second
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            // Handle exception appropriately, e.g., log it or throw it
-        }
-
-        try {
-            Files.write(path, textToAppend.getBytes(), StandardOpenOption.APPEND);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
